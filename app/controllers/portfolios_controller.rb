d4084266d59f0205e1a260c5a17dd3399264198d
Daniellukyanchuk <daniellukyanchuk@gmail.com>
2025-04-25 16:40:00 +0600

    implemented show functionality for portfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,6 +7,10 @@ class PortfoliosController < ApplicationController
 		@portfolio_item = Portfolio.new
 	end
 
+	def show
+		@portfolio_item = Portfolio.find(params[:id])
+	end
+
 	def create
 	    @portfolio_item = Portfolio.new(params.expect(portfolio: [ :title, :subtitle, :body ]))
 
@@ -27,7 +31,7 @@ class PortfoliosController < ApplicationController
 
   	def update
   		@portfolio_item = Portfolio.find(params[:id])
-  		
+
 	    respond_to do |format|
 	      if @portfolio_item.update(params.expect(portfolio: [ :title, :subtitle, :body ]))
 	        format.html { redirect_to portfolios_path, notice: "portfolio_item was successfully updated." }
