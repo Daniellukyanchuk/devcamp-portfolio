4b538eb0af10d1049a4ec790b0268b16f7fb3f2a
Daniellukyanchuk <daniellukyanchuk@gmail.com>
2025-06-02 16:15:19 +0600

    added session tracker for dynamic source links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   include DeviseWhitelist  
+
+  before_action :set_source
+
+  def set_source
+    session[:source] = params[:q] if params[:q]
+  end
 end
