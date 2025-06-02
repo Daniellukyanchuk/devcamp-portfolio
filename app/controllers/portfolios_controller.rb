601efe53e011e8ceb80939d5d4787289395f1d6a
Daniellukyanchuk <daniellukyanchuk@gmail.com>
2025-06-02 16:41:42 +0600

    refactored strong params for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -17,12 +17,7 @@ class PortfoliosController < ApplicationController
 	end
 
 	def create
-	    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(
-		    :title,
-		    :subtitle,
-		    :body,
-		    technologies_attributes: [:id, :name]
-	  	))	
+	    @portfolio_item = Portfolio.new(portfolio_params)	
 
 	    respond_to do |format|
 	      if @portfolio_item.save
@@ -43,7 +38,7 @@ class PortfoliosController < ApplicationController
   		@portfolio_item = Portfolio.find(params[:id])
 
 	    respond_to do |format|
-	      if @portfolio_item.update(params.expect(portfolio: [ :title, :subtitle, :body ]))
+	      if @portfolio_item.update(portfolio_params)
 	        format.html { redirect_to portfolios_path, notice: "portfolio_item was successfully updated." }
 	        format.json { render :show, status: :ok, location: @portfolio_item }
 	      else
@@ -60,5 +55,19 @@ class PortfoliosController < ApplicationController
 	    respond_to do |format|
 	      format.html { redirect_to portfolios_path, status: :see_other, notice: "Blog was successfully destroyed." }
 	    end
+  	end
+
+  	private 
+
+  	def portfolio_params
+  		params.require(:portfolio).permit(
+		    :title,
+		    :subtitle,
+		    :body,
+		    technologies_attributes: [
+		    	:id, 
+		    	:name
+		    ]
+	  	)
   	end
 end
