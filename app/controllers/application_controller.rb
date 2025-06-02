ed344fc2120ae6cfa0ecc499c686db83edf46cf0
Daniellukyanchuk <daniellukyanchuk@gmail.com>
2025-06-02 16:29:43 +0600

    refactored app controller to include concern for our source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,5 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   include DeviseWhitelist  
-
-  before_action :set_source
-
-  def set_source
-    session[:source] = params[:q] if params[:q]
-  end
+  include SetSource
 end
