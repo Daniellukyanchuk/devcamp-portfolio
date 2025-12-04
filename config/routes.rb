1df401debc5cb2a410a68181aec0566129c3cd08
Daniellukyanchuk <daniellukyanchuk@gmail.com>
2025-12-04 15:48:53 +0600

    implemented validations for blogs, skills and portfolio items

--- config/routes.rb
+++ config/routes.rb
@@ -1,40 +1,15 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-  resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-
-  
   resources :blogs do 
     member do
       get :toggle_status
     end
   end
 
-
   root to: 'pages#home'
->>>>>>> Stashed changes
-=======
-  resources :portfolios, except: [:show]
-  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-
-  get 'about-me', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
-
-
-  
-  resources :blogs
-  root to: 'pages#home'
->>>>>>> ca471c508a0119f14d9eef2fc19082ec48b983fa
 end
